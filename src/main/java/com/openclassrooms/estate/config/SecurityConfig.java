fa329056a0ea36c51a2d3ab85849c2dcfb3a8b23
NicolaCo <nicolas.collin42@gmail.com>
2026-08-31 17:02:45 +0200

    fix(image): allow access to image

--- src/main/java/com/openclassrooms/estate/config/SecurityConfig.java
+++ src/main/java/com/openclassrooms/estate/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/api/auth/login",
                                 "/api/auth/register",
+                                "/images/**",
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
